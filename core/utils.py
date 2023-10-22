57e08c63915e632fc2ffe1c49ef629937160c4ec
amir <aamirjafari11@gmail.com>
2023-10-22 08:52:18 +0330

    Defining the function 'translate' in the file

--- core/utils.py
+++ core/utils.py
@@ -1,6 +1,17 @@
+from django.utils.translation import activate
+
 from rest_framework import permissions
+from rest_framework.request import Request
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+
+def translate(request: Request):
+    try:
+        request.LANGUAGE_CODE = request.headers['Accept_Language']
+        activate(request.LANGUAGE_CODE)
+    except:
+        request.LANGUAGE_CODE = 'en-us'
 
 
 # swagger
@@ -16,4 +27,3 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=[permissions.AllowAny],
 )
-
